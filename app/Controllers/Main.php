23062254953e12b6220540748fc19e6ce3e5f7a3
shyestfern <zalesak_maxmilian@oauh.cz>
2026-09-14 11:36:37 +0200

    Zprovoznění indexu s layoutem

---
+++ app/Controllers/Main.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+use Psr\Log\LoggerInterface;
+
+
+class Main extends BaseController
+{
+    public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
+    {
+        parent::initController($request, $response, $logger);
+    }
+
+    public function index(){
+        
+    }
+}
