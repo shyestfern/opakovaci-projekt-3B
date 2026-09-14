2e9106a873120d7a1ea94b40d7f2d85bdeafe2f2
shyestfern <zalesak_maxmilian@oauh.cz>
2026-09-14 12:24:26 +0200

    Vykreslení tabulky s datem

--- app/Controllers/Main.php
+++ app/Controllers/Main.php
@@ -7,15 +7,33 @@ use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
 use Psr\Log\LoggerInterface;
 
+use App\Models\RaceYear;
 
 class Main extends BaseController
 {
+    private object $raceYear;
+    private array $data;
+
     public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
     {
         parent::initController($request, $response, $logger);
+
+        $this->raceYear = new RaceYear();
     }
 
     public function index(){
-        
+        $id = 124;
+
+        $zavod = $this->raceYear
+        ->select('race_year.id_race, race_year.real_name, race_year.start_date, race_year.end_date')
+        ->where('race_year.id_race', $id)
+        ->orderBy('race_year.year', 'desc')
+        ->findAll();
+
+        $this->data = [
+            'zavod' => $zavod
+        ];
+
+        echo view('index', $this->data);
     }
 }
--- app/Views/index.php
+++ app/Views/index.php
@@ -2,4 +2,40 @@
 
 <?= $this->section("content") ?>
 
+<?php
+    $table = new \CodeIgniter\View\Table();
+    $table->setHeading("Název", "Datum");
+
+    /**@var array $zavod */
+
+    foreach($zavod as $row){
+        $datum = $row->start_date . ' - ' . $row->end_date;
+        $table->addRow($row->real_name, $datum);
+    }
+
+    $template = array(
+        'table_open'=> '<table class="table table-bordered table-hover">',
+        'thead_open'=> '<thead class="table-dark">',
+        'thead_close'=> '</thead>',
+        'heading_row_start'=> '<tr>',
+        'heading_row_end'=>' </tr>',
+        'heading_cell_start'=> '<th>',
+        'heading_cell_end' => '</th>',
+        'tbody_open' => '<tbody>',
+        'tbody_close' => '</tbody>',
+        'row_start' => '<tr>',
+        'row_end'  => '</tr>',
+        'cell_start' => '<td>',
+        'cell_end' => '</td>',
+        'row_alt_start' => '<tr>',
+        'row_alt_end' => '</tr>',
+        'cell_alt_start' => '<td>',
+        'cell_alt_end' => '</td>',
+        'table_close' => '</table>',
+    );
+
+    $table->setTemplate($template);
+    echo $table->generate();
+?>
+
 <?= $this->endSection() ?>
